42081acaeadb4cc00d88c88368a446acf38080f4
Andrew Varnerin <andrew@varnerin.info>
2014-07-06 20:57:35 -0400

    add Stamen tilelayer to protect my poor wallet

--- build/app.js
+++ build/app.js
@@ -94,11 +94,37 @@ var bostonBiking;
             var deferred = this.$q.defer();
             this.mapPromise = deferred.promise;
 
-            var lMap = L.mapbox.map('map-canvas', ambient.mapKey);
-            lMap.on('load', function () {
-                var map = new Map(lMap);
-                deferred.resolve(map);
-            });
+            var lMap;
+            if (false) {
+                lMap = L.mapbox.map('map-canvas', ambient.mapKey);
+                lMap.on('load', function () {
+                    var map = new Map(lMap);
+                    deferred.resolve(map);
+                });
+            } else {
+                lMap = L.mapbox.map('map-canvas');
+
+                var tl = L.tileLayer('http://{s}tile.stamen.com/terrain/{z}/{x}/{y}.png', {
+                    'type': 'png',
+                    'subdomains': [
+                        'a.',
+                        'b.',
+                        'c.',
+                        'd.'
+                    ],
+                    'minZoom': 12,
+                    'maxZoom': 18,
+                    'attribution': 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+                });
+                lMap.options.maxZoom = 18;
+                lMap.options.minZoom = 12;
+                tl.addTo(lMap);
+                lMap.setView([42.3490298, -71.0619507], 15);
+                tl.on('load', function () {
+                    var map = new Map(lMap);
+                    deferred.resolve(map);
+                });
+            }
 
             return this.mapPromise;
         };
